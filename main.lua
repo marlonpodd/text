30db13215c5176b2d5ae2f5677c7b8fc756de959
marlonpodd <47573256+marlonpodd@users.noreply.github.com>
2019-02-14 13:18:44 -0500

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,32 @@
+
+-----------------------------------------------------------------------------------------
+--
+-- how to be a memer
+-- 
+-----------------------------------------------------------------------------------------
+
+local myText = display.newText( "MAARRLLOOOON", 160, 100, native.systemFont, 35 )
+myText:setFillColor( 0, 1, 0 )
+
+-- Set default screen background color to blue
+display.setDefault( "background", 100, 1, 0 )
+
+-- note that the image is saved in a directory ./assests/sprites/
+local image = display.newImageRect( "Assets/lamb.png", 	260, 100 )
+image.x = 160
+image.y = 15
+
+local myText = display.newText( "yeet", 160, 240, native.systemFont, 20 )
+myText:setFillColor( 255, 0, 0 )
+
+local image = display.newImageRect( "Assets/yeetus.jpg", 	260, 100 )
+image.x = 160
+image.y = 175
+
+local image = display.newImageRect( "Assets/sanic.png", 	200, 200 )
+image.x = 100
+image.y = 430
+
+local image = display.newImageRect( "Assets/roblox.png", 	250, 250 )
+image.x = 250
+image.y = 350
